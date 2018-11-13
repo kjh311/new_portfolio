af73744bb0f6ca73516850903b55bcb4ba8335f4
Kevin <kjh311@gmail.com>
2018-11-13 08:26:55 -0800

    smooth scroll

---
+++ notes.md
@@ -0,0 +1,41 @@
+* Sticky Nav Bar that grows at top, shrinks as user scrolls down
+
+* SVG animation
+
+* parallax?
+
+* animated icon on hover
+
+##For Projects I want to showcase to a potential employer that I can:
+* DESIGN websites
+* showcase Animation and Transistions (UI/UX developer)
+* Solve proplems (JS)(age gate/)
+* Tie into API's
+
+
+https://coolors.co
+https://www.bestfolios.com/home
+https://medium.com/@adaptivepath/jedi-principles-of-ui-animation-2b88423b1dac
+https://www.sitepoint.com/how-to-create-a-portfolio-site-that-will-get-you-hired/
+
+Cool animations portfolio:
+http://findmatthew.com/
+https://codepen.io/drewendly/full/xEKadN
+https://www.behance.net/gallery/45319371/Save-Green-Landing-Page
+
+Interesting:
+http://kyleledbetter.com/
+https://dribbble.com/shots/4353012-Bryan-Lane-Personal-portfolio/attachments/991061
+
+Indesign tut
+https://www.youtube.com/watch?v=cRNGIoBlzjs
+
+Shrink navbar on scroll:
+https://www.w3schools.com/howto/howto_js_navbar_shrink_scroll.asp
+
+Photoshop web design:
+https://www.youtube.com/watch?v=GKRQC4fLNHs
+
+Greensock fires scroll
+https://greensock.com/forums/topic/17220-can-gsap-animation-be-tied-to-scroll/
+https://greensock.com/forums/topic/6182-start-an-animation-based-on-scroll-position/
---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+
+
+
+
+
+ // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.padding = "0px 10px";
+    document.getElementById("navbar-brand").style.fontSize = "25px";
+    document.getElementById("navbar-toggler").style.width = "66px";
+    document.getElementById("navbar-toggler").style.height = "40px";
+    document.getElementById("navbar-toggler-icon").style.height = "30px";
+    document.getElementById("navbar-toggler-icon").style.height = "30px";
+  } else {
+    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("navbar-brand").style.fontSize = "35px";
+    document.getElementById("navbar-toggler").style.width = "80px";
+    document.getElementById("navbar-toggler").style.height = "55px";
+    document.getElementById("navbar-toggler-icon").style.height = "40px";
+    document.getElementById("navbar-toggler-icon").style.height = "40px";
+  }
+}
+
+
+
+
+
+
+
+// Collapse navbar dropdown on click
+ $(function(){ 
+     var navMain = $(".navbar-collapse"); 
+     var home = $(".navbar-brand");
+     navMain.on("click", "a:not([data-toggle])", null, function () {
+         navMain.collapse('hide');
+     });
+     home.on("click", function () {
+         navMain.collapse('hide');
+     });
+ });
+
+});
