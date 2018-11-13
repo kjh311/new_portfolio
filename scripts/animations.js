ff90481ef89baeef4b7256b35eb1f8de4ab83a24
Kevin <kjh311@gmail.com>
2018-11-13 14:59:53 -0800

    stars

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,30 +2,30 @@ $( document ).ready(function() {
     // alert( "ready!" );
 
 
-    var tl = new TimelineMax();
-    tl.pause();
+   //  var tl = new TimelineMax();
+   //  tl.pause();
 
-     tl.from('.nav-about', 0.5, {x:-100});
+   //   tl.from('.nav-about', 0.5, {x:-100});
 
 
-    // var navMain = ;
-    $(".navbar-toggler").click(function(){
+   //  // var navMain = ;
+   //  $(".navbar-toggler").click(function(){
          
-    	 if ($(".navbar-toggler").hasClass("collapsed")) {
-			  // Closed
-			  // alert("closed")
-			  tl.reverse()
-			  // tl.pause()
-			  // tl.time(0)
-			  // tl.pause()
-			} else {
-			  // Open
-			  tl.paused(!tl.paused())
-			  // tl.restart();
-			  // alert("open")
-			}
+   //  	 if ($(".navbar-toggler").hasClass("collapsed")) {
+			//   // Closed
+			//   // alert("closed")
+			//   tl.reverse()
+			//   // tl.pause()
+			//   // tl.time(0)
+			//   // tl.pause()
+			// } else {
+			//   // Open
+			//   tl.paused(!tl.paused())
+			//   // tl.restart();
+			//   // alert("open")
+			// }
 
-     });
+   //   });
 
    
 
--- scripts/script.js
+++ scripts/script.js
@@ -12,14 +12,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.padding = "0px 10px";
+    document.getElementById("navbar").style.padding = "5px 10px";
     document.getElementById("navbar-brand").style.fontSize = "25px";
     document.getElementById("navbar-toggler").style.width = "66px";
     document.getElementById("navbar-toggler").style.height = "40px";
     document.getElementById("navbar-toggler-icon").style.height = "30px";
     document.getElementById("navbar-toggler-icon").style.height = "30px";
   } else {
-    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("navbar").style.padding = "20px 10px";
     document.getElementById("navbar-brand").style.fontSize = "35px";
     document.getElementById("navbar-toggler").style.width = "80px";
     document.getElementById("navbar-toggler").style.height = "55px";
